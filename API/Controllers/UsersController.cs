ea2335fea806a8be2a3fd229ee599d29ebb34c53
amitesH-92 <amiteshsingh007@hotmail.com>
2023-02-13 21:53:15 +0530

    API Project

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsersController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+       [HttpGet]
+       public async Task<ActionResult<IEnumerable <AppUser>>> GetUsers()
+       {
+        var Users= await _context.Users.ToListAsync();
+        return    Users ; 
+       }
+    
+       [HttpGet("{id}")]
+       public async Task<ActionResult<AppUser>> GetUser(int id)
+       {
+
+        return await _context.Users.FindAsync(id);
+       }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users{get;set;}
+    }
+}
